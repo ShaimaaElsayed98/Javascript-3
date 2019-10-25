004be023d30b8431102f89647966a22973ccb420
ShaimaaElsayed98 <53016795+ShaimaaElsayed98@users.noreply.github.com>
2019-10-25 16:29:33 +0300

    Add files via upload

---
+++ HW-Week9/main.js
@@ -0,0 +1,50 @@
+// Q1
+
+async function f(x,y) {
+
+    console.log(x+y);
+
+  }
+
+  f(1,2).then();
+
+
+
+  //Q3
+
+  async function calculateSum( x ,y ){
+
+    const sum = x + y;
+
+    if( sum>8 ){
+
+        console.log( "larger than 8")
+
+    }
+    else{
+
+        throw new Error;
+
+    }
+
+}
+
+calculateSum(2,6)
+
+ 
+
+//Q4
+
+
+
+async function writeDataToFile(){  
+
+    await fetch('https://api.github.com/users/id'); 
+
+    let data = data.map(function (x) {
+
+    ( return x * 8;) 
+
+    })};
+
+writeDataToFile(data);
