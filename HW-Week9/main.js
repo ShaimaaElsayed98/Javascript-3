8f7ea9841a5b0280b6e3460a739b20d974445414
ShaimaaElsayed98 <53016795+ShaimaaElsayed98@users.noreply.github.com>
2019-10-25 16:33:29 +0300

    Update main.js

--- HW-Week9/main.js
+++ HW-Week9/main.js
@@ -1,50 +1,30 @@
 // Q1
-
 async function f(x,y) {
-
     console.log(x+y);
-
   }
-
   f(2,6).then();
 
 
 
   //Q3
-
   async function calculateSum( x ,y ){
-
     const sum = x + y;
-
     if( sum>8 ){
-
         console.log( "larger than 8")
-
     }
     else{
-
         throw new Error;
-
     }
-
 }
-
 calculateSum(2,6)
 
  
 
 //Q4
-
-
-
 async function writeDataToFile(){  
 
     await fetch('http://myapi.com'); 
 
-    let data = data.map(function (x) {
-
-    ( return x * 8;) 
-
-    })};
+    let data = data.map(function (x) { ( return x * 8;) }};
 
 writeDataToFile(data);
